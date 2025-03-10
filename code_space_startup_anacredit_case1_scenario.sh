7f2ee754b1b81f75e65e884dbd56a7dee59747e3
RBirdwatcher <neil@birdsoftwaresolutions.com>
2025-03-10 16:03:47 +0000

    Choose the most approprate institutional sector for test case 1 #26
    https://github.com/regcommunity/FreeBIRD/issues/26

--- code_space_startup_anacredit_case1_scenario.sh
+++ code_space_startup_anacredit_case1_scenario.sh
@@ -7,5 +7,5 @@ python -m pip install pytest==8.3.4 --quiet
 python -m pip install pytest-xdist==3.6.1 --quiet
 python -m pip install ruff==0.9.7 --quiet
 python -m pip install unidecode==1.3.8 --quiet
-python pybirdai/utils/run_tests.py --uv "False" --reg-tid "F_05_01_REF_FINREP_3_0" --dp-suffix "152589_REF" --dp-value 83491250 --scenario  "scenario_2"
+python pybirdai/utils/run_tests.py --uv "False" --reg-tid "F_05_01_REF_FINREP_3_0" --dp-suffix "152589_REF" --dp-value 83491250 --scenario  "loan_and_guarantee_scenario_1"
 python manage.py runserver
