ffc70e1f98c24d2935d3e15a9119a0c0676c0a47
RBirdwatcher <neil@birdsoftwaresolutions.com>
2025-03-17 08:02:27 +0000

    fix issue with repeated datapoint execution #31
    https://github.com/regcommunity/FreeBIRD/issues/31

--- birds_nest/pybirdai/process_steps/pybird/orchestration.py
+++ birds_nest/pybirdai/process_steps/pybird/orchestration.py
@@ -23,46 +23,57 @@ class Orchestration:
 		object_id = id(theObject)
 		if object_id in Orchestration._initialized_objects:
 			print(f"Object of type {theObject.__class__.__name__} already initialized, skipping.")
+			# Even if we're skipping full initialization, we still need to ensure references are set
+			self._ensure_references_set(theObject)
 			return
 		
 		# Mark this object as initialized
 		Orchestration._initialized_objects.add(object_id)
 		
+		# Set up references for the object
+		self._ensure_references_set(theObject)
+
+	def _ensure_references_set(self, theObject):
+		"""
+		Ensure that all table references are properly set for the object.
+		This is called both during full initialization and when initialization is skipped.
+		"""
 		references = [method for method in dir(theObject.__class__) if not callable(
 		getattr(theObject.__class__, method)) and not method.startswith('__')]
 		for eReference in references:
 			if eReference.endswith("Table"):
+				# Only set the reference if it's currently None
+				if getattr(theObject, eReference) is None:
+					from django.apps import apps
+					table_name = eReference.split('_Table')[0]
+					relevant_model = None
+					try:
+						relevant_model = apps.get_model('pybirdai',table_name)
+					except LookupError:
+						print("LookupError: " + table_name)
 
-				from django.apps import apps
-				table_name = eReference.split('_Table')[0]
-				relevant_model = None
-				try:
-					relevant_model = apps.get_model('pybirdai',table_name)
-				except LookupError:
-					print("LookupError: " + table_name)
+					if relevant_model:
+						print("relevant_model: " + str(relevant_model))
+						newObject = relevant_model.objects.all()
+						print("newObject: " + str(newObject))
+						if newObject:
+							setattr(theObject,eReference,newObject)
+							CSVConverter.persist_object_as_csv(newObject,True);						
+						
+					else:
+						newObject = Orchestration.createObjectFromReferenceType(eReference);
+						
+						operations = [method for method in dir(newObject.__class__) if callable(
+							getattr(newObject.__class__, method)) and not method.startswith('__')]
+						
+						for operation in operations:
+							if operation == "init":
+								try:
+									getattr(newObject, operation)()
+								except:
+									print (" could not call function called " + operation)
 
-				if relevant_model:
-					print("relevant_model: " + str(relevant_model))
-					newObject = relevant_model.objects.all()
-					print("newObject: " + str(newObject))
-					if newObject:
 						setattr(theObject,eReference,newObject)
-						CSVConverter.persist_object_as_csv(newObject,True);						
-					
-				else:
-					newObject = Orchestration.createObjectFromReferenceType(eReference);
-					
-					operations = [method for method in dir(newObject.__class__) if callable(
-						getattr(newObject.__class__, method)) and not method.startswith('__')]
-					
-					for operation in operations:
-						if operation == "init":
-							try:
-								getattr(newObject, operation)()
-							except:
-								print (" coud not call function called " + operation)
-
-					setattr(theObject,eReference,newObject)
 
 	@classmethod
 	def reset_initialization(cls):
